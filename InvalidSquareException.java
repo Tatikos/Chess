199e800092121c15ec4425df49a8318a3fe61a60
Tatikos <87193130+Tatikos@users.noreply.github.com>
2025-07-16 21:59:54 +0300

    Create InvalidSquareException.java

---
+++ InvalidSquareException.java
@@ -0,0 +1,17 @@
+package hw4.chess;
+
+/**
+ * The InvalidSquareException class extends the Exception class and represents
+ * an exception that is thrown when an invalid square is encountered in the game
+ * of chess.
+ */
+public class InvalidSquareException extends Exception {
+	/**
+	 * Constructs a new InvalidSquareException with the given square.
+	 *
+	 * @param square the square that caused the exception
+	 */
+	public InvalidSquareException(String square) {
+		super("Invalid square " + square);
+	}
+}
