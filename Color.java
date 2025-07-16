3b4cec39c2dd40120e054b0814b78d7296720bf7
Tatikos <87193130+Tatikos@users.noreply.github.com>
2025-07-16 21:59:27 +0300

    Create Color.java

---
+++ Color.java
@@ -0,0 +1,14 @@
+package hw4.chess;
+/**
+ * The Color enum represents the two possible colors of a piece in the game of chess.
+ */
+public enum Color {
+	/**
+     * Represents the color white.
+     */
+	WHITE,
+	/**
+     * Represents the color black.
+     */
+	BLACK
+}
